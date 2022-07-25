168af175995e840831ea80e4a322792a29b63a06
x21034 <108244089+x21034@users.noreply.github.com>
2022-07-25 10:07:48 +0900

    dessinに追加

--- Dessin.js
+++ Dessin.js
@@ -1,66 +1,22 @@
-const DSC_1025src = 'url("img/DSC_1025.JPG")';
-const DSC_1026src = 'url("img/DSC_1026.JPG")';
-const DSC_1027src = 'url("img/DSC_1027.JPG")';
-const DSC_1028src = 'url("img/DSC_1028.JPG")';
-const DSC_1029src = 'url("img/DSC_1029.JPG")';
-const DSC_1030src = 'url("img/DSC_1030.JPG")';
-const header = document.getElementById('top');
+var photo = document.getElementById("photo");
 
-// 次の画像を指定する関数
-function nextImage() {
-  const nowBg = header.style.backgroundImage;
-  console.log(nowBg);
-
-  if (nowBg == DSC_1025src) {
-    // 今1枚め
-    header.style.backgroundImage = DSC_1026src;
-  } else if (nowBg == DSC_1026src) {
-    // 今2枚め
-    header.style.backgroundImage = DSC_1027src;
-  } else if (nowBg == DSC_1027src) {
-    // 今3枚め
-    header.style.backgroundImage = DSC_1028src;
-  } else if (nowBg == DSC_1028src) {
-    // 今4枚め
-    header.style.backgroundImage = DSC_1029src;
-  } else if (nowBg == DSC_1029src) {
-    // 今5枚め
-    header.style.backgroundImage = DSC_1030src;
-  } else {
-    // 今6枚め
-    header.style.backgroundImage = DSC_1025src;
-  }
-}
-
-// 前の画像を指定する関数
-function prevImage() {
-  const nowBg = header.style.backgroundImage;
-  console.log(nowBg);
-
-  if (nowBg == DSC_1025src) {
-    // 今1枚め
-    header.style.backgroundImage = DSC_1030src;
-  } else if (nowBg == DSC_1026src) {
-    // 今2枚め
-    header.style.backgroundImage = DSC_1025src;
-  } else if (nowBg == DSC_1027src) {
-    // 今3枚め
-    header.style.backgroundImage = DSC_1026src;
-  } else if (nowBg == DSC_1028src) {
-    // 今4枚め
-    header.style.backgroundImage = DSC_1027src;
-  } else if (nowBg == DSC_1029src) {
-    // 今5枚め
-    header.style.backgroundImage = DSC_1028src;
-  } else {
-    // 今6枚め
-    header.style.backgroundImage = DSC_1029src;
-  }
-}
-
-// ページが表示されたタイミングで実行したい処理を書く
-document.getElementById('top').style.backgroundImage = image01src;
-document.getElementById('sc-to-left').addEventListener('click', prevImage);
-document.getElementById('sc-to-right').addEventListener('click', nextImage);
-// タイマーセット(5秒ごとに次の画像)
-setInterval(nextImage, 5000);
+document.getElementById("photo-1")
+        .addEventListener("click", function() {
+    photo.src = "DSC_1026.JPG";
+});
+document.getElementById("photo-2")
+        .addEventListener("click", function() {
+    photo.src = "DSC_1027.JPG";
+});
+document.getElementById("photo-3")
+        .addEventListener("click", function() {
+    photo.src = "DSC_1028.JPG";
+});
+document.getElementById("photo-4")
+        .addEventListener("click", function() {
+    photo.src = "DSC_1029.JPG";
+});
+document.getElementById("photo-5")
+        .addEventListener("click", function() {
+    photo.src = "DSC_1039.JPG";
+});
